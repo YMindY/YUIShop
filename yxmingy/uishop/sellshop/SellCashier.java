5be1087322a104e6cfd16f8a2ee8c16915c52a92
YxMingY <526817087@qq.com>
2019-02-17 16:57:16 +0800

    推

--- yxmingy/uishop/sellshop/SellCashier.java
+++ yxmingy/uishop/sellshop/SellCashier.java
@@ -1,13 +1,12 @@
 package yxmingy.uishop.sellshop;
 
 import cn.nukkit.Player;
-import yxmingy.yupi.*;
-
-import java.util.*;
-
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import java.util.*;
+import java.util.regex.Pattern;
+import yxmingy.yupi.*;
 
 public class SellCashier extends HandlerBase{
   Map<String,Object> idata;
@@ -22,8 +21,19 @@ public class SellCashier extends HandlerBase{
     @SuppressWarnings("serial")
 		Object[] pdata = gson.fromJson(data,new TypeToken<Object[]>() {
 		}.getType());
-    for(Object p : pdata) {
-    	player.sendMessage(String.valueOf(p));
+    if(!Pattern.matches("^[\\+]?[\\d]+$", String.valueOf(pdata[1]))){
+      player.sendMessage("请输入有效正整数!");
+      return;
     }
+    try{
+      int count = Integer.parseInt(String.valueOf(pdata[1]));
+      int price = Integer.parseInt(String.valueOf(idata.get("价格")));
+    }catch(NumberFormatException e)
+    {
+      player.sendMessage("未知错误!通知服主并让服主通知开发者!");
+      e.printStackTrace();
+      return;
+    }
+    
   }
 }
